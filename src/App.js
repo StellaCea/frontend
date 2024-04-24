bcac1a290e97e71e96a7fc39c8fc9c78d16f115b
StellaCea <stela.ceaicovscaia@hotmail.com>
2024-04-24 22:38:04 +0200

    styled ShopCategory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Shop} from './Pages/Shop';
-import {ShopCategory} from './Pages/ShopCategory';
-import {Product} from './Pages/Product';
+import { Shop } from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
 import {Cart} from './Pages/Cart';
 import {LoginSignup} from './Pages/LoginSignup';
 import { Footer } from './Components/Footer/Footer';
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -8,8 +8,8 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt="" />
-      <div className="shopcategory-ondexSort">
+      <img className='shopcategory-banner' src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span>out of 36 products
         </p>
@@ -19,7 +19,7 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {all_product.map((item,i) => {
-          if (props.category ===item-category){
+          if (props.category=== item.category){
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
           }
           else{
@@ -27,9 +27,11 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
-      
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
     </div>
   )
 }
 
-export default ShopCategory
+export default ShopCategory;
